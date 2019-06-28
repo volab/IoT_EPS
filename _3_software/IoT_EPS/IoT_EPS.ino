334337d3f39ddf3b22acd04f395486f08869008b
MajorLee <joel@vorobotics.com>
2019-06-28 23:52:43 +0200

    ping : WIP

--- _3_software/IoT_EPS/IoT_EPS.ino
+++ _3_software/IoT_EPS/IoT_EPS.ino
@@ -57,6 +57,10 @@ In station mode, when WIFI is not reachable, it switchs in softAP mode and WIFI 
 #include "ESP8266FtpServer.h"
 FtpServer ftpSrv;
 
+extern "C"
+{
+  #include <lwip/icmp.h> // needed for icmp packet definitions
+}
 
 
 
@@ -524,13 +528,98 @@ void setup(){
     }
 
     sysStatus.initCBITTimer();
+    /////////////////////////////////////////////////////////////////////////////
+    //  Pinger test                                                            //
+    ///////////////////////////////////////////////////////////////////////////// 
+  pinger.OnReceive([](const PingerResponse& response)
+  {
+    if (response.ReceivedResponse)
+    {
+      Serial.printf(
+        "Reply from %s: bytes=%d time=%lums TTL=%d\n",
+        response.DestIPAddress.toString().c_str(),
+        response.EchoMessageSize - sizeof(struct icmp_echo_hdr),
+        response.ResponseTime,
+        response.TimeToLive);
+    }
+    else
+    {
+      Serial.printf("Request timed out.\n");
+    }
 
-    // DSPL( dPrompt + "pinging : " + WiFi.dnsIP().toString() );
-    // if(pinger.Ping(WiFi.dnsIP()) == false){
-        // DSPL( dPrompt + "Error during last ping command.");
-    // } else {
-        // DSPL( dPrompt + "internet test target Reachable");
-    // }
+    // Return true to continue the ping sequence.
+    // If current event returns false, the ping sequence is interrupted.
+    return true;
+  });
+  
+  pinger.OnEnd([](const PingerResponse& response)
+  {
+    // Evaluate lost packet percentage
+    float loss = 100;
+    if(response.TotalReceivedResponses > 0)
+    {
+      loss = (response.TotalSentRequests - response.TotalReceivedResponses) * 100 / response.TotalSentRequests;
+    }
+    
+    // Print packet trip data
+    Serial.printf(
+      "Ping statistics for %s:\n",
+      response.DestIPAddress.toString().c_str());
+    Serial.printf(
+      "    Packets: Sent = %lu, Received = %lu, Lost = %lu (%.2f%% loss),\n",
+      response.TotalSentRequests,
+      response.TotalReceivedResponses,
+      response.TotalSentRequests - response.TotalReceivedResponses,
+      loss);
+
+    // Print time information
+    if(response.TotalReceivedResponses > 0)
+    {
+      Serial.printf("Approximate round trip times in milli-seconds:\n");
+      Serial.printf(
+        "    Minimum = %lums, Maximum = %lums, Average = %.2fms\n",
+        response.MinResponseTime,
+        response.MaxResponseTime,
+        response.AvgResponseTime);
+    }
+    
+    // Print host data
+    Serial.printf("Destination host data:\n");
+    Serial.printf(
+      "    IP address: %s\n",
+      response.DestIPAddress.toString().c_str());
+    if(response.DestMacAddress != nullptr)
+    {
+      Serial.printf(
+        "    MAC address: " MACSTR "\n",
+        MAC2STR(response.DestMacAddress->addr));
+    }
+    if(response.DestHostname != "")
+    {
+      Serial.printf(
+        "    DNS name: %s\n",
+        response.DestHostname.c_str());
+    }
+
+    return true;
+  });
+  
+  
+    if ( WiFi.getMode() == WIFI_STA && WiFi.status() == WL_CONNECTED ){
+        // DSPL( dPrompt + "pinging : " + WiFi.dnsIP().toString() );
+        DSPL( dPrompt + "pinging : 8.8.8.8" );
+        // if (pinger.Ping(WiFi.dnsIP()) == false){
+            // for test:
+        IPAddress ip;
+        ip.fromString( "8.8.8.8" );
+        if (pinger.Ping( ip ) == false){
+            DSPL( dPrompt + "Error during last ping command.");
+        } else {
+            DSPL( dPrompt + "internet test target Reachable");
+        }    
+  delay(10000);        
+    }
+
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -563,7 +652,7 @@ void loop(){
     //NTP and RTC test
     if (sysStatus.ntpEnabled){
         rtc.update(); //this check NTP access and update sysStatus
-        if (sysStatus.ntpErr.isErr() ) cParam.write2Json( "ntpErr", "ON" );
+        // if (sysStatus.ntpErr.isErr() ) cParam.write2Json( "ntpErr", "ON" );
     }
 /** @todo check internet connection health */  
 
