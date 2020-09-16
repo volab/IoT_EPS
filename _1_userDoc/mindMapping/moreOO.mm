d3cb4e91a6a3dd6c0bdb6c44178ee55325fccefd
Jo <joel@vorobotics.com>
2020-09-16 21:22:23 +0200

    ajout loreOO.mm

--- _1_userDoc/mindMapping/moreOO.mm
+++ _1_userDoc/mindMapping/moreOO.mm
@@ -2,8 +2,8 @@
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
 <node TEXT="more OO" FOLDED="false" ID="ID_180563910" CREATED="1600199457979" MODIFIED="1600199475327" STYLE="oval">
 <font SIZE="18"/>
-<hook NAME="MapStyle">
-    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+<hook NAME="MapStyle" zoom="1.5">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
@@ -63,6 +63,137 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="CWifiLink" POSITION="right" ID="ID_29959505" CREATED="1600199552535" MODIFIED="1600199603626">
+<edge COLOR="#ff0000"/>
+<node TEXT="but" FOLDED="true" ID="ID_797550368" CREATED="1600203162931" MODIFIED="1600203167667">
+<node TEXT="principalement : remplacer le bloc linéaire de code compris entre les lignes 361 et 491" ID="ID_172563353" CREATED="1600203221009" MODIFIED="1600203292700"/>
+</node>
+<node TEXT="Tentative de passage de WiFi par référence à la méthode begin" ID="ID_1294715846" CREATED="1600204742372" MODIFIED="1600204767013"/>
+<node TEXT="wificred" ID="ID_1493104360" CREATED="1600199642552" MODIFIED="1600199647881">
+<node TEXT="Classe Credential (credential.h .cpp)" ID="ID_473009695" CREATED="1600200334088" MODIFIED="1600200353209"/>
+<node TEXT="attributs prive ?" ID="ID_806420739" CREATED="1600199839113" MODIFIED="1600199851355"/>
+<node TEXT="10 occurences dans .ino" FOLDED="true" ID="ID_1466082625" CREATED="1600199649834" MODIFIED="1600199658188">
+<node TEXT="entre les lignes 360 et 455" ID="ID_242389009" CREATED="1600199807211" MODIFIED="1600201209774"/>
+<node TEXT="92: declaration" ID="ID_1952830312" CREATED="1600201233004" MODIFIED="1600201239569"/>
+<node TEXT="361: begin" ID="ID_78626763" CREATED="1600200905572" MODIFIED="1600200912200"/>
+<node TEXT="362: sysStatus" ID="ID_55746714" CREATED="1600200913245" MODIFIED="1600200947043"/>
+<node TEXT="419: if ready" FOLDED="true" ID="ID_471842192" CREATED="1600200951018" MODIFIED="1600200971164">
+<node TEXT="420: affichage" ID="ID_402510063" CREATED="1600200975810" MODIFIED="1600201010325"/>
+<node TEXT="421" OBJECT="java.lang.Long|421" ID="ID_59274836" CREATED="1600201020137" MODIFIED="1600201022540"/>
+<node TEXT="423: try softAP mode" ID="ID_1771193526" CREATED="1600201332686" MODIFIED="1600201342700"/>
+<node TEXT="424: affichage softAP" ID="ID_631531707" CREATED="1600201027561" MODIFIED="1600201059529"/>
+</node>
+<node TEXT="454: wifi.begin try to join..." ID="ID_1026953075" CREATED="1600201117427" MODIFIED="1600201151040"/>
+<node TEXT="455: try to joint" ID="ID_637437546" CREATED="1600201154495" MODIFIED="1600201172746"/>
+</node>
+<node TEXT="7 occurences dans configParam.cpp" FOLDED="true" ID="ID_1164113822" CREATED="1600199746569" MODIFIED="1600199762430">
+<node TEXT="entre les lignes 274 et 282" ID="ID_1677824606" CREATED="1600200195205" MODIFIED="1600200256838"/>
+<node TEXT="sous la forme d&apos;une instance dans une seule méthode en temps que variables locale" ID="ID_1681807927" CREATED="1600200205047" MODIFIED="1600200284570"/>
+</node>
+<node TEXT="wificred.begin()" FOLDED="true" ID="ID_1559092426" CREATED="1600199861685" MODIFIED="1600200294627">
+<node TEXT="en realité readFromJs" ID="ID_694539940" CREATED="1600200527635" MODIFIED="1600200541412"/>
+<node TEXT="utilise SPIFFS" ID="ID_555797609" CREATED="1600200542056" MODIFIED="1600200702119"/>
+</node>
+</node>
+<node TEXT="sysStatus" ID="ID_145136455" CREATED="1600200706220" MODIFIED="1600200716514">
+<node TEXT="CSysStatus *_pSysStat" ID="ID_1198327924" CREATED="1600205350212" MODIFIED="1600205370108"/>
+<node TEXT="begin(..., CSysStatus *psysStat, ..." ID="ID_1511050275" CREATED="1600205372409" MODIFIED="1600205407908"/>
+</node>
+<node TEXT="WiFi" FOLDED="true" ID="ID_1059330802" CREATED="1600201365124" MODIFIED="1600203153995">
+<node TEXT="globale autoinstanciée" ID="ID_990258601" CREATED="1600201370764" MODIFIED="1600201381678">
+<node TEXT="WiFiClass" ID="ID_1306949843" CREATED="1600203566650" MODIFIED="1600203578002"/>
+<node TEXT="je dirait plutot : extern ESP8266WiFiClass WiFi;" ID="ID_197070188" CREATED="1600203811276" MODIFIED="1600203864292"/>
+</node>
+<node TEXT="C:\MountWD\Donnees\ODJ\008_iao_wrk\Arduino\Croquis\hardware\esp8266com\esp8266\libraries\ESP8266WiFi\src\ESP8266WiFi.h" ID="ID_447621854" CREATED="1600203835328" MODIFIED="1600203850969"/>
+<node TEXT="C:\Program Files (x86)\Arduino\libraries\WiFi\src\WiFi.cpp" ID="ID_1965741163" CREATED="1600201502165" MODIFIED="1600201502165"/>
+<node TEXT="C:\Program Files (x86)\Arduino\libraries\WiFi\src\WiFi.h" FOLDED="true" ID="ID_1229705534" CREATED="1600201540785" MODIFIED="1600203153993">
+<node TEXT="L244" ID="ID_314047236" CREATED="1600201546455" MODIFIED="1600201556761"/>
+<node ID="ID_1626062098" CREATED="1600201567356" MODIFIED="1600201567356"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <div style="color: #ffffff; background-color: #002451; font-family: Consolas, Courier New, monospace; font-weight: normal; font-size: 14px; line-height: 19px; white-space: pre">
+      <div>
+        <span style="color: #ebbbff"><font color="#ebbbff">extern</font></span><span style="color: #ffffff"><font color="#ffffff">&nbsp;</font></span><span style="color: #ffeead"><font color="#ffeead">WiFiClass</font></span><span style="color: #ffffff"><font color="#ffffff">&nbsp;</font></span><span style="color: #ff9da4"><font color="#ff9da4">WiFi</font></span><span style="color: #ffffff"><font color="#ffffff">;</font></span>
+      </div>
+    </div>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="4 occurences dans configParam" ID="ID_1790418031" CREATED="1600202111473" MODIFIED="1600202124348"/>
+<node TEXT="35 occurence dans .ino" ID="ID_395393358" CREATED="1600202127177" MODIFIED="1600202159808">
+<node TEXT="6 occurences entre 201 et 212" ID="ID_1308057552" CREATED="1600202292015" MODIFIED="1600202338495">
+<node TEXT="201:prompt mac address" ID="ID_13649891" CREATED="1600202162517" MODIFIED="1600202183428"/>
+<node TEXT="202: test == mac adresses" ID="ID_1700426875" CREATED="1600202190075" MODIFIED="1600202206406"/>
+<node TEXT="205write2json mac adressee" ID="ID_612395114" CREATED="1600202210474" MODIFIED="1600202231239"/>
+<node TEXT="208: softap mac adresse" ID="ID_226397605" CREATED="1600202231810" MODIFIED="1600202246042"/>
+<node TEXT="209 test ==  softAp mac add" ID="ID_1025676463" CREATED="1600202246810" MODIFIED="1600202264790"/>
+<node TEXT="212 write2json softap mac add" ID="ID_854539888" CREATED="1600202265337" MODIFIED="1600202279026"/>
+</node>
+<node TEXT="11 occ entre 368 et 406" FOLDED="true" ID="ID_118649818" CREATED="1600202583595" MODIFIED="1600202631824">
+<node TEXT="368: setautoconnect" FOLDED="true" ID="ID_1571438308" CREATED="1600202319672" MODIFIED="1600202390033">
+<node TEXT="369 comment" ID="ID_39466027" CREATED="1600202398248" MODIFIED="1600202403154"/>
+</node>
+<node TEXT="371 prompt autoconnect" ID="ID_395271734" CREATED="1600202410488" MODIFIED="1600202423103"/>
+<node TEXT="372 isconnected" ID="ID_1349575995" CREATED="1600202424032" MODIFIED="1600202450866"/>
+<node TEXT="373 prersistent false" ID="ID_593912021" CREATED="1600202457646" MODIFIED="1600202468372"/>
+<node TEXT="374 comment" ID="ID_418947039" CREATED="1600202634069" MODIFIED="1600202641268"/>
+<node TEXT="375 wifi mode" ID="ID_1799840863" CREATED="1600202469208" MODIFIED="1600202486438"/>
+<node TEXT="403 - 406 comment 4 occ" ID="ID_1147248178" CREATED="1600202487307" MODIFIED="1600202555447"/>
+</node>
+<node TEXT="11 occ entre 414 et 482" FOLDED="true" ID="ID_1918278269" CREATED="1600203035396" MODIFIED="1600203128735">
+<node TEXT="414 softAPConfig" ID="ID_1884240927" CREATED="1600202653233" MODIFIED="1600202665860"/>
+<node TEXT="423 softAP(SSID, pass) ready ?" ID="ID_1830317993" CREATED="1600202681422" MODIFIED="1600202776037"/>
+<node TEXT="425 myIP=" ID="ID_750940424" CREATED="1600202779527" MODIFIED="1600202792062"/>
+<node TEXT="442 comment" ID="ID_587127701" CREATED="1600202814424" MODIFIED="1600202819676"/>
+<node TEXT="443 comment" ID="ID_1052288822" CREATED="1600202820788" MODIFIED="1600202839177"/>
+<node TEXT="451 station config" ID="ID_1146437002" CREATED="1600202885829" MODIFIED="1600202911789"/>
+<node TEXT="454begin SSID, pass" ID="ID_16712864" CREATED="1600202840635" MODIFIED="1600202884475"/>
+<node TEXT="458 wait connect ...." ID="ID_1452382312" CREATED="1600202914922" MODIFIED="1600202958059"/>
+<node TEXT="471 if connected" ID="ID_1939127088" CREATED="1600202971458" MODIFIED="1600202990616"/>
+<node TEXT="473 staip =" ID="ID_1380640226" CREATED="1600202999050" MODIFIED="1600203006733"/>
+<node TEXT="482 else disconnect" ID="ID_686284901" CREATED="1600203008315" MODIFIED="1600203091289"/>
+</node>
+<node TEXT="6 occ entre 538 et 616" ID="ID_1271622780" CREATED="1600203446400" MODIFIED="1600203461077">
+<node TEXT="538 getMode" ID="ID_895176385" CREATED="1600203129446" MODIFIED="1600203143602"/>
+<node TEXT="538 getmode" ID="ID_1337548245" CREATED="1600203332003" MODIFIED="1600203341569"/>
+<node TEXT="539 connected?" ID="ID_228227890" CREATED="1600203345735" MODIFIED="1600203354696"/>
+<node TEXT="615 getmode ? STA ou sta et ap 2 occ" ID="ID_749775104" CREATED="1600203355694" MODIFIED="1600203406269"/>
+<node TEXT="616 connected?" ID="ID_412977788" CREATED="1600203407159" MODIFIED="1600203420655"/>
+</node>
+<node TEXT="812 display Mode" ID="ID_421724095" CREATED="1600203494841" MODIFIED="1600203516497"/>
+</node>
+</node>
+<node TEXT="cParam" ID="ID_1986553309" CREATED="1600201537237" MODIFIED="1600205045609">
+<node TEXT="Dans CServerWeb" ID="ID_119966235" CREATED="1600205167613" MODIFIED="1600205180066">
+<node ID="ID_1625443615" CREATED="1600205161683" MODIFIED="1600205161683"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <div style="color: #ffffff; background-color: #002451; font-family: Consolas, Courier New, monospace; font-weight: normal; font-size: 14px; line-height: 19px; white-space: pre">
+      <div>
+        <span style="color: #ffeead"><font color="#ffeead">ConfigParam</font></span><span style="color: #ffffff"><font color="#ffffff">&nbsp;</font></span><span style="color: #99ffff"><font color="#99ffff">*</font></span><span style="color: #ff9da4"><font color="#ff9da4">_pcParam</font></span><span style="color: #ffffff"><font color="#ffffff">&nbsp;</font></span><span style="color: #99ffff"><font color="#99ffff">=</font></span><span style="color: #ffffff"><font color="#ffffff">&nbsp;</font></span><span style="color: #ffc58f"><font color="#ffc58f">nullptr</font></span><span style="color: #ffffff"><font color="#ffffff">;</font></span>
+      </div>
+    </div>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="    void init( CRtc * rtc, ConfigParam *cparam, CPowerPlug *plugs&#xa;                , bool *restartTempoLed, ESP8266WiFiClass *pWifiCon );" ID="ID_564490902" CREATED="1600205215773" MODIFIED="1600205259523" MAX_WIDTH="11.1 cm" MIN_WIDTH="11.1 cm"/>
+</node>
+</node>
+<node TEXT="simpleManualModeChaser();" ID="ID_1381864370" CREATED="1600205117059" MODIFIED="1600205121399"/>
+</node>
 </node>
 </map>
